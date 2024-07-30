3c609ea79a5e03e9fe74f6c14302a7622cc302e4
Rishabh Gorakhpuriya <rishabhgorakhpuriya@gmail.com>
2024-07-30 22:26:26 +0530

    files

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,64 +1,3 @@
-// const express = require('express');
-// const { sendPushNotification } = require('../controller/notificationSender');
-// const { sendSMS } = require('../controller/smsSender');
-// const { sendEmail } = require('../controller/emailSender');
-// // const { sendFlightStatusUpdate } = require('../controller/kakfaProducer'); // Corrected path
-// const { sendMessage } = require('../controller/rabbitmqProducer');
-// const Flight = require('../models/flightStatus');
-// const Notification = require('../models/notification');
-
-// const router = express.Router();
-
-// // Middleware to parse JSON bodies for this router
-// router.use(express.json()); // No need to use `bodyParser`, `express.json()` is sufficient
-
-// // Endpoint to post flight status updates
-// router.post('/flights/:flight_id', async (req, res) => {
-//     const { flight_id } = req.params;
-//     const { status } = req.body;
-//     try {
-//         const flight = await Flight.findOneAndUpdate(
-//             { flight_id },
-//             { status, last_updated: new Date() },
-//             { new: true, upsert: true }
-//         );
-
-//         const notifications = await Notification.find({ flight_id });
-//         notifications.forEach(async (notification) => {
-//             switch (notification.method) {
-//                 case 'Email':
-//                     await sendEmail(notification.recipient, notification.message);
-//                     break;
-//                 case 'SMS':
-//                     await sendSMS(notification.recipient, notification.message);
-//                     break;
-//                 case 'App':
-//                     await sendPushNotification(notification.recipient, notification.message);
-//                     break;
-//             }
-//         });
-
-//         // sendFlightStatusUpdate(JSON.stringify({ flight_id, status }));
-//         sendMessage(JSON.stringify({ flight_id, status }));
-
-//         res.status(200).json(flight);
-//     } catch (error) {
-//         res.status(500).send('Error updating flight status');
-//     }
-// });
-
-// // Endpoint to manage notifications
-// router.post('/notifications', async (req, res) => {
-//     const notifications = req.body; 
-//     try {
-//         await Notification.insertMany(notifications);
-//         res.status(200).send('Notifications saved successfully');
-//     } catch (error) {
-//         res.status(500).send('Error saving notifications');
-//     }
-// });
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const Notification = require('../models/notification');
@@ -75,7 +14,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Configure Twilio
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const twilioClient = twilio('', '');
 
 router.post('/send', async (req, res) => {
     const { flight_id } = req.body;
